09490bff12d10fe837c66c354ce462062f66acf2
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-16 06:30:33 +0800

    First pass at getting the thing working

--- composite.go
+++ composite.go
@@ -1,29 +1,98 @@
 package fcomposite
 
 import(
+    "fmt"
+    "bytes"
+    "net/http"
     "github.com/ricallinson/forgery"
 )
 
-type Map map[string]func(*f.Request, *f.Response, func())
+// The Writer used in place of stackr.Writer for buffering.
+type BufferedResponseWriter struct {
+    Headers http.Header
+    Buffer *bytes.Buffer
+    Status int
+}
+
+// Header returns the header map that will be sent by WriteHeader.
+// Changing the header after a call to WriteHeader (or Write) has
+// no effect.
+func (this *BufferedResponseWriter) Header() (http.Header) {
+    if this.Headers == nil {
+        this.Headers = http.Header{}
+    }
+    return this.Headers
+}
+
+// Write writes the data to the connection as part of an HTTP reply.
+// If WriteHeader has not yet been called, Write calls WriteHeader(http.StatusOK)
+// before writing the data.  If the Header does not contain a
+// Content-Type line, Write adds a Content-Type set to the result of passing
+// the initial 512 bytes of written data to DetectContentType.
+func (this *BufferedResponseWriter) Write(b []byte) (int, error) {
+    if this.Buffer == nil {
+        this.Buffer = &bytes.Buffer{}
+    }
+    len, err := this.Buffer.Write(b)
+    fmt.Println(string(this.Buffer.Bytes()))
+    return len, err
+}
+
+// WriteHeader sends an HTTP response header with status code.
+// If WriteHeader is not called explicitly, the first call to Write
+// will trigger an implicit WriteHeader(http.StatusOK).
+// Thus explicit calls to WriteHeader are mainly used to
+// send error codes.
+func (this *BufferedResponseWriter) WriteHeader(code int) {
+    this.Status = code
+}
 
 /*
     composite := fcomposite.Map{
         "header": func(req, res, next) {
-            res.End("Header string")
+            res.Send("Header string")
         },
         "body": func(req, res, next) {
-            res.End("Body string")
+            res.Render("page.html", "Body string")
         },
         "footer": func(req, res, next) {
             res.End("Footer string")
+        },
+        "tail": func(req, res, next) {
+            res.Write("Tail string")
+        },
+        "close": func(req, res, next) {
+            res.WriteBytes([]byte("Close string"))
         },
     }
 
     data := composite.Dispatch(req, res, next)
 */
-func (this *Map) Dispatch(req *f.Request, res *f.Response, next func()) (map[string]string) {
+type Map map[string]func(*f.Request, *f.Response, func())
 
-    // Replace res.Writer with something to buffer the data.
+/*
+    The worker.
+*/
+func (this Map) Dispatch(req *f.Request, res *f.Response, next func()) (map[string][]byte) {
 
-    return map[string]string{}
+    done := map[string][]byte{}
+
+    // Grab the res.Writer so we can put it back later.
+    w := res.Response.Writer
+
+    for id, fn := range this {
+        // Create a buffer.
+        buffer := &BufferedResponseWriter{}
+        // Replace res.Writer with BufferedResponseWriter so all the output can be captured.
+        res.Response.Writer = buffer
+        // Call the function.
+        fn(req, res, next)
+        // Add the buffered data to the done map.
+        done[id] = buffer.Buffer.Bytes()
+    }
+
+    // Put the res.Writer back.
+    res.Response.Writer = w
+
+    return done
 }
--- composite_test.go
+++ composite_test.go
@@ -1,17 +1,67 @@
 package fcomposite
 
 import(
-    // "fmt"
     "testing"
+    "net/url"
+    "net/http"
+    "github.com/ricallinson/stackr"
+    "github.com/ricallinson/forgery"
     . "github.com/ricallinson/simplebdd"
 )
 
 func TestFcomposite(t *testing.T) {
 
+    var mock *MockResponseWriter
+    var req *f.Request
+    var res *f.Response
+
+    BeforeEach(func() {
+        mock = NewMockResponseWriter(false)
+        req = &f.Request{
+            Request: &stackr.Request{
+                Request: &http.Request{
+                    URL: &url.URL{},
+                    Header: map[string][]string{},
+                },
+                Query: map[string]string{},
+            },
+        }
+        res = &f.Response{
+            Response: &stackr.Response{
+                Writer: mock,
+            },
+            Charset: "utf-8",
+            Locals: map[string]string{},
+        }
+    })
+
     Describe("Dispatch()", func() {
 
         It("should return", func() {
-            AssertEqual(false, true)
+
+            composite := Map{
+                // "header": func(req *f.Request, res *f.Response, next func()) {
+                //     res.Send("Header string")
+                // },
+                // "body": func(req *f.Request, res *f.Response, next func()) {
+                //     res.Render("page.html", "Body string")
+                // },
+                "footer": func(req *f.Request, res *f.Response, next func()) {
+                    res.End("Footer string")
+                },
+                "tail": func(req *f.Request, res *f.Response, next func()) {
+                    res.Write("Tail string")
+                },
+                "close": func(req *f.Request, res *f.Response, next func()) {
+                    res.WriteBytes([]byte("Close string"))
+                },
+            }
+
+            fn := func(){}
+
+            data := composite.Dispatch(req, res, fn)
+
+            AssertEqual(data, true)
         })
     })
 
